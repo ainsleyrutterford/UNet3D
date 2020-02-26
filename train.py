bed883c4442e0599294cb835263c19ea0e1c5e5f
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2020-02-26 20:01:28 +0000

    Add DataLoader

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import data
 import model
 import torch.optim as optim
+from torch.utils.data import DataLoader
 
 unet = model.UNet3D()
 
@@ -8,4 +9,6 @@ optimizer = optim.Adam(unet.parameters(), lr=0.0001)
 
 batch_size = 2
 
-train_set = data.CoralDataset2D(sample_dir="data/image", label_dir="data/label")
+train_set = data.CoralDataset2D(sample_dir="data/train/image", label_dir="data/train/label")
+
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)

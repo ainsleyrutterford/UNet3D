7069e545e8141776527f755d463d6f8053b58861
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2020-02-26 22:12:53 +0000

    Fix CoralDataset2D

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import glob
 import os
+from skimage import io
 from torch.utils.data import Dataset, DataLoader
 
 class CoralDataset2D(Dataset):
@@ -14,15 +15,16 @@ class CoralDataset2D(Dataset):
         return len(glob.glob(f"{self.sample_dir}/*.png"))
 
     def __getitem__(self, idx):
-        f = sorted(glob.glob(f"{self.sample_dir}/*.png")[idx])
-        name = os.path.basename(f)
+        f = sorted(glob.glob(f"{self.sample_dir}/*.png"))[idx]
+        name = os.path.abspath(f)
         sample = io.imread(name)
 
-        f = sorted(glob.glob(f"{self.label_dir}/*.png")[idx])
-        name = os.path.basename(f)
+        f = sorted(glob.glob(f"{self.label_dir}/*.png"))[idx]
+        name = os.path.abspath(f)
         label = io.imread(name)
 
         if self.transform:
             sample = self.transform(sample)
+            label = self.transform(label)
 
-        return {'sample': torch.tensor(sample), 'label': torch.tensor(label)}
+        return {'sample': sample, 'label': label}
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import data
 import model
 import torch.optim as optim
+import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 unet = model.UNet3D()
@@ -9,6 +10,12 @@ optimizer = optim.Adam(unet.parameters(), lr=0.0001)
 
 batch_size = 2
 
-train_set = data.CoralDataset2D(sample_dir="data/train/image", label_dir="data/train/label")
+train_set = data.CoralDataset2D(sample_dir="data/train/image", 
+                                label_dir="data/train/label",
+                                transform=transforms.Compose([transforms.ToTensor()]))
 
 train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+for batch in train_loader:
+    print(batch['sample'].shape)
+    print(batch['label'].shape)
